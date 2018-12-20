fa9f8870ed275eb2dc4c4008f2af701af6b6c9ec
Hesham Medhat <heshammedhat5@gmail.com>
2018-12-20 16:38:27 +0200

    Implement elements for hashing process

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,6 +2,7 @@
 #define USERPROG_PROCESS_H
 
 #include <list.h>
+#include <hash.h>
 
 typedef int tid_t;
 
@@ -12,9 +13,28 @@ struct process_data {
     struct list file_descriptors;           /* File descriptors acquired from kernel */
 };
 
+
+/* Elements for a hashing process information */
+
+/* Structure for the hashed element */
+struct process_hash {
+    struct hash_elem elem;          /* Hash element for donated priorities
+                                       table. */
+    int key;                        /* Key for finding this process */
+
+    tid_t id;                       /* Identifier. */
+    struct thread *t;               /* Pointer to thread if exists */
+};
+/* Hashing function for process_hash structure */
+unsigned process_hash_func (const struct hash_elem *, void *);
+/* Comparator function for hash_struct that returns true if a precedes b */
+bool process_hash_less (const struct hash_elem *,
+                                 const struct hash_elem *, void *);
+
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
 
 #endif /* userprog/process.h */
