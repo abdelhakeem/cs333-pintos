39503f427168848ceb49021b0e0af242c74e4624
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2018-12-22 15:08:55 +0200

    Make parent wait for child in exec

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -85,6 +85,7 @@ struct thread
   {
     /* Owned by thread.c. */
     tid_t tid;                          /* Thread identifier. */
+    tid_t ptid;                         /* Parent identifier. */
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
